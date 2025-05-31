90fd8c521c1af75debf459c0e7176bb6b131c318
saschadueser <saschadueser@gmail.com>
2025-05-31 17:17:43 +0200

    Added more tabindex support
    Auto Setting tabindex for chosen elements on the website +++ setting Keydown EventHandler for navigation Trigger

--- events.js
+++ events.js
@@ -1,30 +1,59 @@
 // Simple animation for the header navigation
 
-// Navigation elements
+// collection of elements
 const navTrigger = document.querySelector("header .fa");
 const navigation = document.querySelector("header nav");
 const navigationItems = document.querySelectorAll("header nav a");
+const navigationItemsTabIndexes = [];
+const teaserButton = document.querySelector("#teaser .button");
+
+const navigationEventsToActivate = ["click", "keydown"];
 
 // navigation EventHandler
-navTrigger.addEventListener("click", function(e) {
-    navigation.classList.toggle("active");
-    e.target.classList.toggle("fa-xmark");
-});
-
+navigationEventsToActivate.forEach( kindOfEvent => {
+    navTrigger.addEventListener(`${kindOfEvent}`, (e) => {
+        // EventHandler for open oder close navigation
+        if(kindOfEvent === "click" || e.key == "Enter") {
+            navigation.classList.toggle("active");
+            e.target.classList.toggle("fa-xmark");
+        }
+        // Handling the tabindex depending if nav is visible or not
+        if(navigation.classList.contains("active")) {
+            let i = 0;
+            navigationItems.forEach( el => {
+                el.setAttribute("tabindex", navigationItemsTabIndexes[i++])
+            })
+        } else {
+            navTabIndexReset();
+        }
+    })
+})
 
 // Setting Tabindexes
 
 // Which elements should get tabIndexes
-const tabIndexElements = [navTrigger, navigationItems];
+const tabIndexElements = [navTrigger, navigationItems, teaserButton];
 
-// Set the tabindex
+// Set the tabindex, anker points for using Tab Key
 let currentTabIndex = 0;
 tabIndexElements.forEach( el => {
     if( el.length > 1) {
         el.forEach(li => {
-            li.setAttribute("tabindex", ++currentTabIndex)
+            li.setAttribute("tabindex", ++currentTabIndex);
         });
     } else {
         el.setAttribute("tabindex", ++currentTabIndex);
     }
 })
+
+// function to reset the tabindex of navigation items
+function navTabIndexReset() {
+    navigationItems.forEach( el => {
+        navigationItemsTabIndexes.push(el.getAttribute("tabindex"));
+        el.setAttribute("tabindex", -1)
+    })
+};
+
+// auto load to reset tabindex in nav-items - invisible at beginning
+window.addEventListener("load", navTabIndexReset);
+
