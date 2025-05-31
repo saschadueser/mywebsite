4368ac2c01e8d11603bd838060a7f4a806b7a58a
saschadueser <saschadueser@gmail.com>
2025-05-31 18:34:38 +0200

    Added regular lazy loading for sections
    Added regular lazy loading function for all section content elements on the page

---
+++ lazy-loading.js
@@ -0,0 +1,30 @@
+/*  
+    ***
+    Observer for lazy loading all sections of the page
+    ***
+*/
+
+// Elements to observe
+const sections = document.querySelectorAll("section .content");
+
+// Options set
+const options = {
+    threshold: 0.5,
+    rootMargin: "100px"
+}
+
+// Observer function
+let observer = new IntersectionObserver( entries => {
+    entries.forEach( entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.remove("allHidden");
+            entry.target.classList.add("visible")
+            observer.unobserve(el);
+        }
+    })
+}, options);
+
+// Setting observer to all section elements
+sections.forEach( el => {
+    observer.observe(el);
+});
