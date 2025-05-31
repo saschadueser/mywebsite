af264e1b6cd906bf8352dd29a84fffaaa0b9d25e
saschadueser <saschadueser@gmail.com>
2025-05-31 10:08:25 +0200

    Added Tabindex function
    Created first layout for setting tabindexes for chosen elements

--- events.js
+++ events.js
@@ -3,9 +3,28 @@
 // Navigation elements
 const navTrigger = document.querySelector("header .fa");
 const navigation = document.querySelector("header nav");
+const navigationItems = document.querySelectorAll("header nav a");
 
 // navigation EventHandler
 navTrigger.addEventListener("click", function(e) {
     navigation.classList.toggle("active");
     e.target.classList.toggle("fa-xmark");
 });
+
+
+// Setting Tabindexes
+
+// Which elements should get tabIndexes
+const tabIndexElements = [navTrigger, navigationItems];
+
+// Set the tabindex
+let currentTabIndex = 0;
+tabIndexElements.forEach( el => {
+    if( el.length > 1) {
+        el.forEach(li => {
+            li.setAttribute("tabindex", ++currentTabIndex)
+        });
+    } else {
+        el.setAttribute("tabindex", ++currentTabIndex);
+    }
+})
